251fc8a63d3305efe41614ffcc00c9a96221e77e
Nahim104 <nahimhilsaca1945@gmail.com>
2023-08-18 13:37:32 -0600

    Primer commit

---
+++ src/examenip2_nahimhilsaca/ExamenIP2_NahimHilsaca.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package examenip2_nahimhilsaca;
+
+/**
+ *
+ * @author nahim
+ */
+public class ExamenIP2_NahimHilsaca {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
